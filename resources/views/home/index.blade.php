140bb69ddb75a7ca32196c5805eb79cf65914e84
黃文輝 <antory040512@gmail.com>
2023-10-20 10:26:07 +0800

    建立樣板架構

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,5 +7,11 @@
 <body>
     <h1>歡迎來到我的網站</h1>
     <p>這是一個簡單的HTML網頁。</p>
+    @extends('layouts.master')
+    @section('title','Home')
+    @section('content')
+    <h1>Home</h1>
+    <p>This is my body content.</p>
+    @endsection
 </body>
 </html>
