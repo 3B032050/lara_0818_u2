c8939ed9fc181741fe1a173ff6a139b7e5d0ba18
黃文輝 <antory040512@gmail.com>
2023-11-21 18:19:12 +0800

    刪除home/index.blade.php裡的html架構

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>home</title>
-</head>
-<body>
-    <h1>歡迎來到我的網站</h1>
-    <p>這是一個簡單的HTML網頁。</p>
     @extends('layouts.master')
     @section('title','Home')
     @section('content')
-    <h1>Home</h1>
-    <p>This is my body content.</p>
     @endsection
-</body>
-</html>
+
